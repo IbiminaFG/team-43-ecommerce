076677eaea95e6ad6a452e5e2b09f8d52d82233a
Avartarr <sunshineokoye6@gmail.com>
2023-11-24 16:56:48 +0100

    second commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { AiOutlineHeart  } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import { RiLightbulbFlashLine } from "react-icons/ri";
@@ -7,9 +7,11 @@ import { MdOutlineLiveHelp } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
 import { HiOutlineUserGroup } from "react-icons/hi";
 import { FcReddit  } from "react-icons/fc";
+import { IoIosClose } from "react-icons/io";
 
 const Sidebar = ({children}) => {
-
+  const[isOpen ,setIsOpen] = useState(true);
+  const toggle = () => setIsOpen (!isOpen);
   const menuItem = [
     {
       path: "/",
@@ -38,12 +40,15 @@ const Sidebar = ({children}) => {
     },
   ];
   return (
-    <div className="sidebar">
+    <div className="sidebar" style={{width: isOpen ? "200px" : "70px"}}>
+      <div className="ml-36">
+      <IoIosClose onClick={toggle} style={{display: isOpen ? "block" : "none"}} size={20} />
+      </div>
       <div className="top">        
-        <div className="bars">
-          <FcReddit size={35} />
+        <div className="bars" style={{marginLeft: isOpen ? "10px" : "0px"}}>
+          <FcReddit size={35} onClick={toggle} />
         </div>
-        <h1 className="text-xl font-bold pt-1">CHOWDECK</h1>
+        <h1 className="text-xl font-bold pt-1" style={{display: isOpen ? "block" : "none"}}>CHOWDECK</h1>
       </div>
       {menuItem.map((item, index) => (
         <NavLink
@@ -53,13 +58,13 @@ const Sidebar = ({children}) => {
           activeClassName="active"
         >
           <div className="">{item.icon}</div>
-          <div className="">{item.name}</div>
+          <div style={{display: isOpen ? "block" : "none"}}>{item.name}</div>
         </NavLink>
       ))}
       <main>{children}</main>
       <div className=" team">
       <HiOutlineUserGroup size={20}/>
-      <h1 className="">Team 43</h1>
+      <h1 className="" style={{display: isOpen ? "block" : "none"}}>Team 43</h1>
       </div>
     </div>
   );
